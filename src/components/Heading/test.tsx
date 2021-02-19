91e2c640489b9e044c969fcc5590b504d5361501
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-02-19 10:01:49 -0300

    add heading

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import 'jest-styled-components'
+import Heading from '.'
+import { renderWithTheme } from '../../utils/tests/helpers'
+
+describe('<Logo />', () => {
+  it('should render white header by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render black header when colors is passed', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+  it('should render a heading with a line below it', () => {
+    renderWithTheme(<Heading lineBottom>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'border-bottom',
+      '0.4rem solid #F231A5',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+})
