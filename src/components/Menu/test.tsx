eb091f479970f19da4406dd06e661cf9c298de01
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-03-02 10:19:52 -0300

    add desktop menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -31,4 +31,17 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+  it('should show wishlist when logged in', () => {
+    renderWithTheme(<Menu username="arthur" />)
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+  })
 })
