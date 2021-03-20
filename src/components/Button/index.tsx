e9e9fb3808ba4b55c3bc680a00a7a5d345f0ffa3
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-03-20 10:09:38 -0300

    add button polimorfism

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react'
 import * as S from './styles'
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
 export type ButtonProps = {
-  children?: React.ReactNode
+  // children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: React.ReactNode
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
-}
+  as?: React.ElementType
+  // onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+} & ButtonTypes
 
 const Button = ({
   children,
