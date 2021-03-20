c478833ef188bf6892f3bd7a027df28206a1992b
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-03-20 09:57:07 -0300

    add plop to generate files

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import Footer from '.'
+
+describe('Footer', () => {
+  it('should render 4 columns topics', () => {
+    //contact, follow us, link location
+  })
+})
