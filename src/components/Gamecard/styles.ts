095f852ab0129a18fe16eb2463c56420040b12b5
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-03-30 23:06:19 -0300

    add style to gamecard

--- src/components/Gamecard/styles.ts
+++ src/components/Gamecard/styles.ts
@@ -8,10 +8,13 @@ export const Wrapper = styled.article`
     width: 100%;
     height: 100%;
     background-color: ${theme.colors.white};
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+    border-radius: 6px;
   `}
 `
 
 export const ImageBox = styled.div`
+  border-radius: 6px 6px 0 0;
   height: 14rem;
   width: 100%;
   background: #f6f7f8;
@@ -28,6 +31,7 @@ export const ImageBox = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    border-radius: 6px 6px 0 0;
   }
   @keyframes placeholderShimmer {
     0% {
