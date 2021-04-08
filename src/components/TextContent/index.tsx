62ef734f4f1a8c9d468a9b579eebad6f01a01e8c
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-08 09:42:01 -0300

    finish text content

---
+++ src/components/TextContent/index.tsx
@@ -0,0 +1,23 @@
+import * as S from './styles'
+import Heading from 'components/Heading'
+
+export type TextContentProps = {
+  title?: string
+  content: string
+}
+
+// create sanitize method for the __html content
+
+const TextContent = ({ title, content }: TextContentProps) => (
+  <S.Wrapper>
+    {!!title && (
+      <Heading lineLeft lineColor="secondary">
+        {title}
+      </Heading>
+    )}
+
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  </S.Wrapper>
+)
+
+export default TextContent
