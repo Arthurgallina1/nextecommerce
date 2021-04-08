36a5a3e9f131e76b1280d7b0c28c923128582ff7
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-08 09:12:03 -0300

    add props to gamedetails and tests

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,15 +1,33 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameDetails from './index'
+import GameDetails, { GameDetailsProps } from './index'
 
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'apple', 'linux'],
+  releaseDate: '2020-11-21T23:00:00'
+}
 describe('GameDetails', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameDetails />)
-    expect(
-      screen.getByRole('heading', { name: /GameDetails/i })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+    expect(screen.getByRole('heading', { name: /developer/i }))
+    expect(screen.getByRole('heading', { name: /release date/i }))
+    expect(screen.getByRole('heading', { name: /platforms/i }))
+    expect(screen.getByRole('heading', { name: /publisher/i }))
+    expect(screen.getByRole('heading', { name: /rating/i }))
+    expect(screen.getByRole('heading', { name: /genres/i }))
   })
 
+  it('should render platforms icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+    expect(screen.getByRole('img', { name: /windows/i }))
+    expect(screen.getByRole('img', { name: /linux/i }))
+    expect(screen.getByRole('img', { name: /mac/i }))
+  })
+
+  it('should render formated date', () => {
+    renderWithTheme(<GameDetails {...props} />)
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
+  })
 })
