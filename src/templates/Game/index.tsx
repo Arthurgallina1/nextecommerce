24ceb62ab91aa660de494b31f2e9b5da35b3e950
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-10 17:53:06 -0300

    add style to game page

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,14 @@
+import Base from 'templates/Base'
+import * as S from './styles'
+
+const Game = () => (
+  <Base>
+    <S.Cover
+      src={'https://source.unsplash.com/user/willianjusten/1042x580'}
+      role="img"
+      aria-label="cover"
+    />
+  </Base>
+)
+
+export default Game
