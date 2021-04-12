c640e023385307cdafaaee6c75dd31c77a26c041
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-11 22:51:42 -0300

    /

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+import Base from 'templates/Base'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="primary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
