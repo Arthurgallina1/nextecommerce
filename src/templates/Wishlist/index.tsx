a76e9201e754d13aa2fe933b8409bad483cdebbd
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-11 22:54:56 -0300

    init wishlsit

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,11 +1,13 @@
 import * as S from './styles'
 
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
 import Base from 'templates/Base'
 
 const Wishlist = () => (
   <Base>
     <Container>
-      <Heading lineLeft lineColor="primary">
+      <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
