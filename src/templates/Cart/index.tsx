f72d53b001778b909a121cdce540f945e519da1a
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-14 22:35:57 -0300

    finish cart

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,6 +1,7 @@
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import { GamecardProps } from 'components/Gamecard'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
@@ -32,11 +33,19 @@ const Cart = ({
         <Heading lineLeft lineColor="secondary">
           My cart
         </Heading>
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <Empty
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
 
-        <S.Content>
-          <CartList items={items} total={total} />
-          <PaymentOptions cards={cards} handlePayment={handlePayment} />
-        </S.Content>
         <Divider />
       </Container>
 
