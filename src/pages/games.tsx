bdc50375e3fc1554e017a0e320bbdbebaa9cc479
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-19 09:47:56 -0300

    populate games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,52 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemMock from 'components/Sidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { gql } from '@apollo/client'
+import { cover } from 'polished'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `
+  })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337/${game?.cover?.url}` || null,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: filterItemMock
     }
   }
 }
+
+// title: 'Population Zero',
+// developer: 'Rockstar Games',
+// img: 'https://source.unsplash.com/user/willianjusten/300x140',
+// price: 'R$ 235,00',
+// promotionalPrice: 'R$ 215,00'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,12 @@
-import { gql, useQuery } from '@apollo/client'
-
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/Bannerslider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
-  const { data, loading, error } = useQuery(gql`
-    query getGames {
-      games {
-        name
-      }
-    }
-  `)
-
-  if (loading) return <p style={{ color: 'white' }}>loading</p>
-
-  if (error) return <p>{error}</p>
-
-  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
-// export function getServerSideProps() {
-//   return {
-//     props: {
-//       heading: 'most popular'
-//     }
-//   }
-// }
-// getServerSideProps -> gera via SSR a cada request (não vai para o bundle do cliente)
-// getInitialProps -> gera via ssr a cada request (vai para o cliente, faz hydrate do lado do cliente depois do 1o req)
-// getStaticProps -> generate static in build time SSG
-export function getStaticProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannersMock,
@@ -47,3 +21,29 @@ export function getStaticProps() {
     }
   }
 }
+
+// export function getServerSideProps() {
+//   return {
+//     props: {
+//       heading: 'most popular'
+//     }
+//   }
+// }
+// getServerSideProps -> gera via SSR a cada request (não vai para o bundle do cliente)
+// getInitialProps -> gera via ssr a cada request (vai para o cliente, faz hydrate do lado do cliente depois do 1o req)
+// getStaticProps -> generate static in build time SSG
+// export function getStaticProps() {
+//   return {
+//     props: {
+//       banners: bannersMock,
+//       newGames: bannersMock,
+//       mostPopularHighlight: highlightMock,
+//       mostPopularGames: bannersMock,
+//       upcomingGames: bannersMock,
+//       upcomingHighligth: highlightMock,
+//       freeGames: bannersMock,
+//       upcomingMoreGames: bannersMock,
+//       freeHighligth: highlightMock
+//     }
+//   }
+// }
