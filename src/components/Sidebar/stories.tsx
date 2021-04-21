0ed8615df0958d1f3aa5a352cd9bd983151b8a42
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-21 11:21:40 -0300

    fix typecheck

--- src/components/Sidebar/stories.tsx
+++ src/components/Sidebar/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Sidebar, { SidebarProps } from '.'
+import Sidebar, { ExploreSidebarProps } from '.'
 
 import items from './mock'
 
@@ -17,13 +17,13 @@ export default {
   }
 } as Meta
 
-export const Default: Story<SidebarProps> = (args) => (
+export const Default: Story<ExploreSidebarProps> = (args) => (
   <div style={{ padding: 16, maxWidth: 320 }}>
     <Sidebar {...args} />
   </div>
 )
 
-export const WithInitialValues: Story<SidebarProps> = (args) => (
+export const WithInitialValues: Story<ExploreSidebarProps> = (args) => (
   <div style={{ padding: 16, maxWidth: 320 }}>
     <Sidebar
       {...args}
