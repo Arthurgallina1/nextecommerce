79490930aca80a5585c8150f5703397eacc6fbc9
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-04-28 10:36:42 -0300

    add new handles for filter in side bar

--- src/components/Sidebar/test.tsx
+++ src/components/Sidebar/test.tsx
@@ -16,21 +16,24 @@ describe('<ExploreSidebar />', () => {
     expect(
       screen.getByRole('heading', { name: /sort by/i })
     ).toBeInTheDocument()
-    expect(screen.getByRole('heading', { name: /system/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
     expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
   })
 
-  // it('should render inputs', () => {
-  //   renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
+  it('should render inputs', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
+    screen.logTestingPlaygroundURL()
 
-  //   expect(
-  //     screen.getByRole('checkbox', { name: /under \$50/i })
-  //   ).toBeInTheDocument()
+    expect(
+      screen.getByRole('checkbox', { name: /under \$50/i })
+    ).toBeInTheDocument()
 
-  //   expect(
-  //     screen.getByRole('radio', { name: /low to high/i })
-  //   ).toBeInTheDocument()
-  // })
+    expect(
+      screen.getByRole('radio', { name: /low to high/i })
+    ).toBeInTheDocument()
+  })
 
   it('should render the filter button', () => {
     renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
@@ -38,19 +41,19 @@ describe('<ExploreSidebar />', () => {
     expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument()
   })
 
-  // it('should check initial values that are passed', () => {
-  //   renderWithTheme(
-  //     <ExploreSidebar
-  //       items={items}
-  //       onFilter={jest.fn}
-  //       initialValues={{ windows: true, sort_by: 'low-to-high' }}
-  //     />
-  //   )
+  it('should check initial values that are passed', () => {
+    renderWithTheme(
+      <ExploreSidebar
+        items={items}
+        onFilter={jest.fn}
+        initialValues={{ platforms: ['windows'], sort_by: 'low-to-high' }}
+      />
+    )
 
-  //   expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked()
+    expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked()
 
-  //   expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked()
-  // })
+    expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked()
+  })
 
   it('should filter with initial values', () => {
     const onFilter = jest.fn()
@@ -58,14 +61,17 @@ describe('<ExploreSidebar />', () => {
     renderWithTheme(
       <ExploreSidebar
         items={items}
-        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        initialValues={{ platforms: ['windows'], sort_by: 'low-to-high' }}
         onFilter={onFilter}
       />
     )
 
     userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
-    expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' })
+    expect(onFilter).toBeCalledWith({
+      platforms: ['windows'],
+      sort_by: 'low-to-high'
+    })
   })
 
   it('should filter with checked values', () => {
@@ -80,8 +86,7 @@ describe('<ExploreSidebar />', () => {
     userEvent.click(screen.getByRole('button', { name: /filter/i }))
 
     expect(onFilter).toBeCalledWith({
-      windows: true,
-      linux: true,
+      platforms: ['windows', 'linux'],
       sort_by: 'low-to-high'
     })
   })
