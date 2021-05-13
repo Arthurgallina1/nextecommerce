bcacddace9fb533ec23519c3ccec2b6e638c55fe
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-05-13 09:55:50 -0300

    add CartButton and tests

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -1,15 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CartDropdown, { CartDropdownProps } from '.'
-
+import CartDropdown from '.'
 import items from 'components/CartList/mock'
 
 export default {
   title: 'CartDropdown',
   component: CartDropdown,
-  args: {
-    items,
-    total: 'R$ 300,00'
-  },
   parameters: {
     backgrounds: {
       default: 'won-dark'
@@ -17,7 +12,21 @@ export default {
   }
 } as Meta
 
-export const Default: Story<CartDropdownProps> = (args) => (
+export const Default: Story = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+)
+
+Default.args = {
+  cartContextValue: {
+    items,
+    quantity: items.length,
+    total: 'R$ 300,00'
+  }
+}
+
+export const Empty: Story = (args) => (
   <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
     <CartDropdown {...args} />
   </div>
