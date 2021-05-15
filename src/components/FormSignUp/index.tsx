e70cf516e49897b3495eadf61f9b074847de2950
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-05-15 11:29:19 -0300

    add sign up

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,46 +1,81 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react'
 import Link from 'next/link'
 import { Email, Lock, AccountCircle } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 import { FormWrapper, FormLink } from 'components/Form'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import { useMutation } from '@apollo/client'
+import { MUTATION_REGISTER } from 'graphql/mutations/register'
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="name"
-        placeholder="Name"
-        type="name"
-        icon={<AccountCircle />}
-      />
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <TextField
-        name="confirm-password"
-        placeholder="Confirm password"
-        type="password"
-        icon={<Email />}
-      />
+const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    username: '',
+    email: '',
+    password: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign up now
-      </Button>
-      <FormLink>
-        Already have an account? <Link href="/sign-in">Sign In</Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const [createUser] = useMutation(MUTATION_REGISTER)
+
+  const handleInputChange = (field: string, value: string) => {
+    setValues((oldValues) => ({ ...oldValues, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    createUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="username"
+          placeholder="Username"
+          type="text"
+          onInputChange={(v) => handleInputChange('username', v)}
+          icon={<AccountCircle />}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          onInputChange={(v) => handleInputChange('email', v)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          onInputChange={(v) => handleInputChange('password', v)}
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+        />
+
+        <Button size="large" fullWidth>
+          Sign up now
+        </Button>
+        <FormLink>
+          Already have an account? <Link href="/sign-in">Sign In</Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -3,7 +3,7 @@ import { useState, InputHTMLAttributes } from 'react'
 import * as S from './styles'
 
 export type TextFieldProps = {
-  onInput?: (value: string) => void
+  onInputChange?: (value: string) => void
   label?: string
   initialValue?: string
   error?: string
@@ -16,7 +16,7 @@ const TextField = ({
   label,
   name,
   initialValue = '',
-  onInput,
+  onInputChange,
   error,
   icon,
   iconPosition = 'left',
@@ -29,7 +29,7 @@ const TextField = ({
     const newValue = e.currentTarget.value
     setValue(newValue)
 
-    !!onInput && onInput(newValue)
+    !!onInputChange && onInputChange(newValue)
   }
 
   return (
