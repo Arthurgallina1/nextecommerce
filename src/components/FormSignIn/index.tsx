30f1bfeb1ab86e29595506b429f307885973c873
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-05-20 09:42:58 -0300

    add sign in and login verification

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,37 +1,72 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { Email, Lock } from '@styled-icons/material-outlined'
-import Button from 'components/Button'
 
+import Button from 'components/Button'
 import TextField from 'components/TextField'
 import { FormWrapper, FormLink } from 'components/Form'
+
 import * as S from './styles'
 
-const FormSignIn = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+const FormSignIn = () => {
+  const { push } = useRouter()
+  const [values, setValues] = useState({
+    username: '',
+    password: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign in now
-      </Button>
-      <FormLink>
-        Don't have an account? <Link href="/sign-up">Sign up</Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const handleInputChange = (field: string, value: string) => {
+    setValues((oldValues) => ({ ...oldValues, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: '/'
+    })
+
+    if (result?.url) {
+      return push(result?.url)
+    }
+
+    console.error('email ou senha invalido')
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="Email"
+          onInputChange={(v) => handleInputChange('email', v)}
+          type="email"
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          onInputChange={(v) => handleInputChange('password', v)}
+          placeholder="Password"
+          type="password"
+          icon={<Lock />}
+        />
+
+        <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+
+        <Button type="submit" size="large" fullWidth>
+          Sign in now
+        </Button>
+        <FormLink>
+          Don't have an account? <Link href="/sign-up">Sign up</Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignIn
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import NextNprogress from 'nextjs-progressbar'
+import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -13,27 +14,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Power House E-Commerce</title>
-            <link rel="shortcut icon" href="/img/man.svg" />
-            <link rel="apple-touch-icon" href="/img/man.svg" />
-            <meta name="description" content="Ultra perfomance E-commerce" />
-            <link rel="manifest" href="/manifest.json" />
-          </Head>
-          <GlobalStyles />
-          <NextNprogress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Power House E-Commerce</title>
+              <link rel="shortcut icon" href="/img/man.svg" />
+              <link rel="apple-touch-icon" href="/img/man.svg" />
+              <meta name="description" content="Ultra perfomance E-commerce" />
+              <link rel="manifest" href="/manifest.json" />
+            </Head>
+            <GlobalStyles />
+            <NextNprogress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+            />
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
