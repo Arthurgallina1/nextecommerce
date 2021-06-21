d7bc4e7e6ad17e7a009ac64263b3f6eecea5380f
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-06-21 09:55:58 -0300

    finish wishlist caching

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -3,7 +3,10 @@ import { GetServerSidePropsContext } from 'next'
 import Profile from 'templates/Profile'
 import FormProfile, { FormProfileProps } from 'components/FormProfile'
 
-import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
+import {
+  QueryProfileMe,
+  QueryProfileMeVariables
+} from 'graphql/generated/QueryProfileMe'
 import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 import { initializeApollo } from 'utils/apollo'
 import protectedRoutes from 'utils/protected-routes'
@@ -20,10 +23,16 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session) //passing the session means its authed
 
-  const { data } = await apolloClient.query<QueryProfileMe>({
-    query: QUERY_PROFILE_ME
+  const { data } = await apolloClient.query<
+    QueryProfileMe,
+    QueryProfileMeVariables
+  >({
+    query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id
+    }
   })
   return {
-    props: { session, username: data.me?.username, email: data.me?.email }
+    props: { session, username: data.user?.username, email: data.user?.email }
   }
 }
