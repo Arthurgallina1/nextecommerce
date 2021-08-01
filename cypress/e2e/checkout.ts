8a33c31d1c597b6363b2606a8554a3f85504643e
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-08-01 11:11:17 -0300

    add e2e free games checkout

---
+++ cypress/e2e/checkout.ts
@@ -0,0 +1,68 @@
+/// <reference path="../support/index.d.ts" />
+
+import { createUser, User } from '../support/generate'
+
+describe('Checkout', () => {
+  let user: User
+
+  describe('Free Games', () => {
+    user = createUser()
+  })
+
+  it('should buy free games', () => {
+    //create user
+    cy.visit('/sign-up')
+    cy.signUp(user)
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    //go to explore page and filter free games
+    cy.findByRole('link', { name: /explore/i }).click()
+    cy.findByText(/free/i).click()
+    cy.url().should('contain', 'price_lte=0')
+
+    //add free game to cart
+    cy.addToCartByIndex(0)
+
+    //verify if cart has 1 game and open dropdown
+    cy.findAllByLabelText(/cart items/i)
+      .first()
+      .should('have.text', 1)
+      .click()
+
+    //click to buy
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByText(/buy it now/i).click()
+    })
+
+    // find free games text
+    cy.findByText(/Only free games, click buy and enjoy/i).should('exist')
+
+    //click to buy
+    cy.findByRole('button', { name: /buy now/i }).click()
+    //be redirect to sucess
+    cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+
+    cy.findByRole('heading', { name: /Your purchase was successful!/i }).should(
+      'exist'
+    )
+  })
+
+  it('should show games in orders page', () => {
+    cy.visit('/profile/orders')
+    //should redir to sign in
+    cy.location('href').should(
+      'eq',
+      `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`
+    )
+
+    cy.signIn(user.email, user.password)
+    cy.location('href').should(
+      'eq',
+      `${Cypress.config().baseUrl}/profile/orders`
+    )
+
+    cy.getByDataCy('game-item').should('have.length', 1)
+  })
+
+  describe('Paid Games', () => {})
+})
